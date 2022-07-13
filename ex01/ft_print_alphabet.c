92245387d62d36c8f710476e8f0d364bd13b723a
Gabriel Bellon <gabriel1490@gmail.com>
2022-07-13 19:40:50 +0100

    ex 01 done (?)

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpadovan <jpadovan@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 19:26:27 by jpadovan          #+#    #+#             */
+/*   Updated: 2022/07/13 19:39:18 by jpadovan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void);
+void	ft_putchar(char c);
+
+void	ft_print_alphabet(void)
+{
+    int x = 'a';
+    while(x <= 'z') {
+		ft_putchar(x);
+		x++;
+    }
+}
+
+void	ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
